49fcb024a69b51b4142467ec027fd2169259ab9e
Pasi Niemi <pasi.niemi@nitor.com>
2026-03-19 13:48:24 +0200

    Config fix

--- custom_components/lg_tv_manager/config_flow.py
+++ custom_components/lg_tv_manager/config_flow.py
@@ -75,10 +75,13 @@ class LgTvManagerConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 class LgTvManagerOptionsFlow(config_entries.OptionsFlow):
     """Handle options flow."""
 
+    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
+        self._config_entry = config_entry
+
     async def async_step_init(self, user_input: dict[str, Any] | None = None) -> FlowResult:
         if user_input is not None:
             return self.async_create_entry(title="", data=user_input)
         return self.async_show_form(
             step_id="init",
-            data_schema=_options_schema(dict(self.config_entry.options)),
+            data_schema=_options_schema(dict(self._config_entry.options)),
         )
